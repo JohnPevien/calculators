a58985e16c29420a21f225d47b42c4b5b1bd410a
John Pevien Laguindam <johnpevienwork@gmail.com>
2022-07-23 23:40:06 +0800

    Update app component to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainLayout from './components/MainLayout';
+import TextField from './components/TextField';
 
-function App() {
+function App(): JSX.Element {
   return (
     <MainLayout>
-      <div className='container-xl flex justify-center'></div>
+      <div className='container-xl flex justify-center'>
+      </div>
     </MainLayout>
   );
 }
